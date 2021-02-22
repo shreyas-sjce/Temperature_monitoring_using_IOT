9fa85c899ce2c19139d7a7b50762066c7c9a530d
root <skyfall8694@gmail.com>
2021-02-22 07:39:36 +0000

    Files are Updated

---
+++ email_conf.py
@@ -0,0 +1,8 @@
+MAILGUN_API_KEY = 'a6ba33077f4e47af802e32f60014d083-19f318b0-d8c2ee72' #This is the private API key which you can find on your Mailgun Dashboard
+SANDBOX_URL= 'sandbox8a687abc7928498bbc4e2599e5eea767.mailgun.org' #You can find this on your Mailgun Dashboard
+SENDER_EMAIL = 'test@' + SANDBOX_URL  # No need to modify this. The sandbox URL is of the format test@YOUR_SANDBOX_URL
+RECIPIENT_EMAIL = 'krishnah60317@gmail.com' #Enter your Email ID Here
+API_KEY = '719f76ad-8955-49ff-92a2-282341aa1bf4' #This is your Bolt Cloud account API key
+DEVICE_ID = 'BOLT1115768' #This is the ID of your Bolt device
+
+ 
